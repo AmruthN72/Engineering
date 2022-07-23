dcb6ab6fe0c3aaaab2576c858141c72988252b62
amruth <amruth.n72@gmail.com>
2022-07-23 20:38:48 +0530

    Implemented binary search

---
+++ Java programs/BinarySearch/src/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch {
+    static int binarySearch(int[] array,int target){
+
+        int i=0,j = array.length,mid;
+        while (i<= j){
+            mid = i+((j-i))/2;
+            if (array[mid] == target){
+                return mid;
+            } else if (target > array[mid]) {
+                i = mid +1;
+            }
+            else {
+                j = mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        //sorted array must be passed for binary search
+        int[] a ={1,2,3,4,5,9};
+       int b= binarySearch(a,9);
+        System.out.println(b);
+    }
+}
